b359e1737dc2b14b3bf79758e2bc678497ddbb1e
Till Theato <till.theato@evocount.de>
2019-01-20 09:56:37 +0100

    Basic implementation.

---
+++ addon/code-scanner/component.js
@@ -0,0 +1,67 @@
+import Component from '@ember/component';
+import { observes } from '@ember-decorators/object';
+import { classNames, layout, tagName } from '@ember-decorators/component';
+import { BrowserQRCodeReader } from '@zxing/library';
+import template from './template';
+
+@layout(template)
+@tagName('video')
+@classNames('code-scanner')
+class CodeScanner extends Component {
+
+  device = undefined;
+
+  didInsertElement() {
+    super.didInsertElement(...arguments);
+
+    this.set('codeReader', new BrowserQRCodeReader());
+
+    this.startVideoScanning();
+
+    this.codeReader.getVideoInputDevices().then(
+      videoInputDevices => {
+        if (this.onDevicesFound) {
+          this.onDevicesFound(videoInputDevices);
+        }
+        videoInputDevices.forEach(
+          device => console.log(`${device.label}, ${device.deviceId}`)
+        );
+      }
+    ).catch(
+      error => {
+        console.log(error);
+        if (this.onDevicesError) {
+          this.onDevicesError(error);
+        }
+      }
+    )
+  }
+
+  @observes('device')
+  onDeviceChange() {
+    this.codeReader.reset();
+    this.startVideoScanning();
+  }
+
+  startVideoScanning() {
+    console.log('starting to scan');
+    this.codeReader.decodeFromInputVideoDevice(
+      this.device,
+      this.elementId
+    ).then(
+      result => {
+        if (this.onScanSuccess) {
+          this.onScanSuccess(result);
+        }
+      }
+    ).catch(
+      error => {
+        if (this.onScanError) {
+          this.onScanError(error);
+        }
+      }
+    );
+  }
+}
+
+export default CodeScanner;
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  options: {
+    babel: {
+      plugins: [ require('ember-auto-import/babel-plugin') ]
+    }
+  },
 };
